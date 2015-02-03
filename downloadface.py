bc5a5270b47a7dbee7338b1c8a8ef3b633775097
Jentery Sayers <jentery@uvic.ca>
2015-02-02 19:47:46 -0800

    changed xml call in download.py to face.xml

--- downloadface.py
+++ downloadface.py
@@ -5,7 +5,7 @@ from urlparse import urlparse
 
 def detect(path):
     img = cv2.imread(path)
-    cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+    cascade = cv2.CascadeClassifier("face.xml")
     rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
 
     if len(rects) == 0:
